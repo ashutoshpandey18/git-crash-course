68017f991ccc595ad9159c0c92d79f04a40152ed
Ashutosh Pandey <ashutoshpandey23june2005@gmail.com>
2024-11-01 04:17:52 -0700

    replace concate with template literals

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const name1 = 'Ashutosh Pandey'
 const dob = '01/01/2005'
 
 function sayName(){
-    console.log('My Name is '+ name1);
+    console.log(`My Name is ${name1}`);
 }
 
 function isAbove18(){
